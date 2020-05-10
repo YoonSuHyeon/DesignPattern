acda15bc34bdbd3967b05fbd912703ae14f13ec2
YoonSuHyeon <96-05-03@naver.com>
2020-05-10 17:31:10 +0900

    MidtermTemplateMethod

---
+++ MidtermTemplateMethod/Electronics.java
@@ -0,0 +1,9 @@
+package MidtermTemplateMethod;
+public class Electronics extends BasicEngineering
+{
+ @Override
+ public void SpecialPaper()
+ {
+ System.out.println("Digital Logic and Circuit Theory");
+ }
+}
